f74a9364162d3a0e2d606312eac438e3a8b8f8dc
Zicklag <zicklag@katharostech.com>
2021-04-27 10:31:03 -0500

    Fix Loading of Fonts With Un-quoted Properties

--- src/property.rs
+++ src/property.rs
@@ -1,27 +1,30 @@
 /// A `Font` property.
 #[derive(PartialEq, Eq, Clone, Debug)]
 pub enum Property {
-	///
-	String(String),
+    ///
+    String(String),
 
-	///
-	Integer(i64),
+    ///
+    Integer(i64),
 }
 
 impl Property {
-	/// Parse a property string.
-	#[inline]
-	pub fn parse(string: &str) -> Property {
-		if string.starts_with('"') {
-			Property::String(extract(string))
-		}
-		else {
-			Property::Integer(string.parse().unwrap())
-		}
-	}
+    /// Parse a property string.
+    #[inline]
+    pub fn parse(string: &str) -> Property {
+        if string.starts_with('"') {
+            Property::String(extract(string))
+        } else {
+            if let Ok(int) = string.parse() {
+                Property::Integer(int)
+            } else {
+                Property::String(string.into())
+            }
+        }
+    }
 }
 
 #[inline]
 pub fn extract(string: &str) -> String {
-	(&string[1 .. string.len() - 1]).replace("\"\"", "\"")
+    (&string[1..string.len() - 1]).replace("\"\"", "\"")
 }
